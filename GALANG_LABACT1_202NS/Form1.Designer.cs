916ed6b0f2aae5099c63057d8078c44b11011559
aizergalang <aizergalang@gmail.com>
2026-01-31 12:10:58 +0800

    Add files via upload

---
+++ GALANG_LABACT1_202NS/Form1.Designer.cs
@@ -0,0 +1,406 @@
+﻿namespace GALANG_LABACT1_202NS
+{
+    partial class X
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblOfficer = new System.Windows.Forms.Label();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOfficer = new System.Windows.Forms.TextBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.lblCenterName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblItemName = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.txtItemName = new System.Windows.Forms.TextBox();
+            this.nudQuantity = new System.Windows.Forms.NumericUpDown();
+            this.dtpReceived = new System.Windows.Forms.DateTimePicker();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblBarangay = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dgvReliefGoods = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReliefGoods)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(502, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Disaster Relief Goods Tracking System";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblOfficer);
+            this.groupBox1.Controls.Add(this.lblLocation);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtOfficer);
+            this.groupBox1.Controls.Add(this.txtLocation);
+            this.groupBox1.Controls.Add(this.lblCenterName);
+            this.groupBox1.Location = new System.Drawing.Point(67, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(329, 262);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Relief Center Information";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lblOfficer
+            // 
+            this.lblOfficer.AutoSize = true;
+            this.lblOfficer.Location = new System.Drawing.Point(27, 139);
+            this.lblOfficer.Name = "lblOfficer";
+            this.lblOfficer.Size = new System.Drawing.Size(109, 17);
+            this.lblOfficer.TabIndex = 5;
+            this.lblOfficer.Text = "Officer-in-Charge";
+            this.lblOfficer.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(24, 76);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(57, 17);
+            this.lblLocation.TabIndex = 4;
+            this.lblLocation.Text = "Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Relief Center Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtOfficer
+            // 
+            this.txtOfficer.Location = new System.Drawing.Point(41, 157);
+            this.txtOfficer.Name = "txtOfficer";
+            this.txtOfficer.Size = new System.Drawing.Size(251, 25);
+            this.txtOfficer.TabIndex = 2;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(41, 96);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(251, 25);
+            this.txtLocation.TabIndex = 1;
+            // 
+            // lblCenterName
+            // 
+            this.lblCenterName.Location = new System.Drawing.Point(41, 43);
+            this.lblCenterName.Name = "lblCenterName";
+            this.lblCenterName.Size = new System.Drawing.Size(251, 25);
+            this.lblCenterName.TabIndex = 0;
+            this.lblCenterName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dtpReceived);
+            this.groupBox2.Controls.Add(this.nudQuantity);
+            this.groupBox2.Controls.Add(this.cboCategory);
+            this.groupBox2.Controls.Add(this.lblDate);
+            this.groupBox2.Controls.Add(this.lblQuantity);
+            this.groupBox2.Controls.Add(this.lblItemName);
+            this.groupBox2.Controls.Add(this.lblCategory);
+            this.groupBox2.Controls.Add(this.txtItemName);
+            this.groupBox2.Location = new System.Drawing.Point(953, 78);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(329, 262);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Goods Inventory Entry";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // cboCategory
+            // 
+            this.cboCategory.FormattingEnabled = true;
+            this.cboCategory.Items.AddRange(new object[] {
+            "Food Packs",
+            "Drinking Water",
+            "Medical Supplies",
+            "Clothing",
+            "Hygiene Kits"});
+            this.cboCategory.Location = new System.Drawing.Point(41, 48);
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Size = new System.Drawing.Size(251, 25);
+            this.cboCategory.TabIndex = 8;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(24, 191);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(91, 17);
+            this.lblDate.TabIndex = 7;
+            this.lblDate.Text = "Date Received";
+            this.lblDate.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(24, 137);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(56, 17);
+            this.lblQuantity.TabIndex = 5;
+            this.lblQuantity.Text = "Quantity\r\n";
+            this.lblQuantity.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblItemName
+            // 
+            this.lblItemName.AutoSize = true;
+            this.lblItemName.Location = new System.Drawing.Point(24, 76);
+            this.lblItemName.Name = "lblItemName";
+            this.lblItemName.Size = new System.Drawing.Size(72, 17);
+            this.lblItemName.TabIndex = 4;
+            this.lblItemName.Text = "Item Name";
+            this.lblItemName.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(24, 23);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(90, 17);
+            this.lblCategory.TabIndex = 3;
+            this.lblCategory.Text = "Item Category";
+            // 
+            // txtItemName
+            // 
+            this.txtItemName.Location = new System.Drawing.Point(41, 96);
+            this.txtItemName.Name = "txtItemName";
+            this.txtItemName.Size = new System.Drawing.Size(251, 25);
+            this.txtItemName.TabIndex = 1;
+            // 
+            // nudQuantity
+            // 
+            this.nudQuantity.Location = new System.Drawing.Point(41, 157);
+            this.nudQuantity.Name = "nudQuantity";
+            this.nudQuantity.Size = new System.Drawing.Size(251, 25);
+            this.nudQuantity.TabIndex = 9;
+            // 
+            // dtpReceived
+            // 
+            this.dtpReceived.Location = new System.Drawing.Point(41, 211);
+            this.dtpReceived.Name = "dtpReceived";
+            this.dtpReceived.Size = new System.Drawing.Size(251, 25);
+            this.dtpReceived.TabIndex = 10;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.lblStatus);
+            this.groupBox3.Controls.Add(this.lblBarangay);
+            this.groupBox3.Location = new System.Drawing.Point(508, 78);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(329, 262);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Distribution Status";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(24, 155);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(114, 17);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Distribution Status";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // lblBarangay
+            // 
+            this.lblBarangay.AutoSize = true;
+            this.lblBarangay.Location = new System.Drawing.Point(24, 51);
+            this.lblBarangay.Name = "lblBarangay";
+            this.lblBarangay.Size = new System.Drawing.Size(131, 17);
+            this.lblBarangay.TabIndex = 3;
+            this.lblBarangay.Text = "Destination Barangay";
+            this.lblBarangay.Click += new System.EventHandler(this.label6_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(41, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(251, 25);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "  ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "In Storage",
+            "Ready for Distribution",
+            "Distributed"});
+            this.comboBox1.Location = new System.Drawing.Point(41, 178);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(251, 25);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dgvReliefGoods
+            // 
+            this.dgvReliefGoods.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReliefGoods.Location = new System.Drawing.Point(67, 373);
+            this.dgvReliefGoods.Name = "dgvReliefGoods";
+            this.dgvReliefGoods.Size = new System.Drawing.Size(1206, 197);
+            this.dgvReliefGoods.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(182, 590);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(177, 54);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add Record";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(469, 590);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(177, 54);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update Record";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(750, 590);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(177, 54);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear Fields";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1031, 590);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(177, 54);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // X
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1376, 829);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvReliefGoods);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "X";
+            this.Text = "Disaster Relief Goods Tracking System";
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReliefGoods)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtOfficer;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.TextBox lblCenterName;
+        private System.Windows.Forms.Label lblOfficer;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblItemName;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.TextBox txtItemName;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.ComboBox cboCategory;
+        private System.Windows.Forms.NumericUpDown nudQuantity;
+        private System.Windows.Forms.DateTimePicker dtpReceived;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblBarangay;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dgvReliefGoods;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ GALANG_LABACT1_202NS/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GALANG_LABACT1_202NS
+{
+    public partial class X : Form
+    {
+        public X()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
